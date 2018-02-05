f1e0345297092396526af70b0df1bbf02126e597
brian boddaert <brianistheloneliestnumber@outlook.com>
2018-02-05 10:49:50 +0100

    Created Models
    Created Subject, Task and User model

---
+++ App_Code/Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Subject
+/// </summary>
+public class Subject
+{
+    String subjectID;
+    String name;
+    String color;
+    public Subject()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ App_Code/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+    String username;
+    String password;
+    public User()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
