ef7c7615cf0d439f5d949f44982dbe7b22eaee74
Paul <pberkhof@student.scalda.nl>
2018-03-05 10:24:09 +0100

    tasks
    You can now add a subject to the task and see the overvieuw of all users. Also the urgency get shown correctly.

--- AllUsers.aspx.cs
+++ AllUsers.aspx.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class AllUsers : System.Web.UI.Page
 {
-
-    public List<Task> allTasks = new List<Task>();
     MySql.Data.MySqlClient.MySqlConnection conn;
     MySql.Data.MySqlClient.MySqlCommand cmd;
     string querStr;
 
+    public List<User> allUsers = new List<User>();
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        allTasks.Clear();
-        getAllTasks();
+        getAllUsers();
+
     }
 
-    public void getAllTasks()
-    {
-        //CONNECT TO DB
+    private void getAllUsers() {
         string connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
 
         conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
@@ -32,25 +28,24 @@ public partial class _Default : System.Web.UI.Page
 
         querStr = "";
 
-        //CREATE A COMMAND
-        querStr = "SELECT * FROM tasks WHERE assignee_id = '" + LoggedInUser.currentUser.UserID + "'";
+        querStr = "SELECT * FROM users";
+
         cmd = new MySql.Data.MySqlClient.MySqlCommand(querStr, conn);
 
         //READ FROM DB
         MySql.Data.MySqlClient.MySqlDataReader reader = cmd.ExecuteReader();
+
+        allUsers.Clear();
+
         while (reader.Read())
         {
-            Task t = new Task();
-            t.name = reader["name"].ToString();
-            t.description = reader["description"].ToString();
-            t.urgency = reader["urgency"].ToString();
-            t.taskType = reader["type"].ToString();
+            User u = new User();
+            u.Username = reader["username"].ToString();
+            u.UserID = Convert.ToInt32(reader["id"].ToString()); 
 
-            allTasks.Add(t);
+            allUsers.Add(u);
         }
 
         conn.Close();
-
     }
-
 }
--- App_Code/Models/Task.cs
+++ App_Code/Models/Task.cs
@@ -11,11 +11,11 @@ public class Task
     public String name;
     public String taskID;
     public Subject subject;
-    public List<User> assignees;
+    public int assignees;
     public DateTime deadline;
     public String description;
     public String urgency;
-
+    public String taskType;
 
     public Task()
     {
@@ -27,7 +27,7 @@ public class Task
     public string Name { get => name; set => name = value; }
     public string TaskID { get => taskID; set => taskID = value; }
     public Subject Subject { get => subject; set => subject = value; }
-    public List<User> Assignees { get => assignees; set => assignees = value; }
+    public int Assignees { get => assignees; set => assignees = value; }
     public DateTime Deadline { get => deadline; set => deadline = value; }
     public string Description { get => description; set => description = value; }
     public string Urgency { get => urgency; set => urgency = value; }
